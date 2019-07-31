04177cae15b8a16f4db1859841ba7719fe22b768
jason.mckim <jason.mckim@spiderstrategies.com>
2019-07-30 21:44:35 -0400

    fix format and escaping

--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 f_name = sys.argv[1]
-expression = r'(?P<ip>.*) \[(?P<date>.*) (?P<timezone>.*?)\] "(?P<request_method>.*) (?P<path>.*) (?P<request_version>HTTP/.*)" (?P<status_code>.*\d) (?P<size>.*) (?P<duration>.*) "(?P<user_agent>.*)" (?P<userid>.*) (?P<sessionid>.*)'
+expression = r'(?P<ip>.*) \[(?P<date>.*) (?P<timezone>.*?)\] "(?P<request_method>.*) (?P<path>.*) (?P<request_version>HTTP/.*)" (?P<status_code>.*\d) (?P<size>.*[\d-]) (?P<duration>.*[\d-]) "(?P<user_agent>.*)" (?P<userid>.*[\d-]) (?P<sessionid>.*)'
 error_expression = r'(?P<ip>.*) \[(?P<date>.*) (?P<timezone>.*?)\] "(?P<path>.*)" (?P<status_code>.*\d) (?P<size>.*) (?P<duration>.*) "(?P<user_agent>.*)" (?P<userid>.*) (?P<sessionid>.*)'
 compiled_regex = re.compile(expression)
 error_compiled_regex = re.compile(error_expression)
@@ -15,12 +15,15 @@ newfile.write(fields + "\n")
 def handleLine(regex, line):
     match = regex.match(l.strip())
     dict = match.groupdict()
+    newline = ""
     for field in fields.split(","):
         if(dict.has_key(field)):
-            newfile.write("\"" + dict[field] + "\",")
+            val = dict[field]
+            val = val.replace("\"","\\\"")
+            newline = newline + ("\"" + val + "\",")
         else:
-            newfile.write("\" \",")
-    newfile.write("\n")
+            newline = newline + ("\" \",")
+    newfile.write(newline + "\n")
 
 with open(f_name) as f:
     for l in f:
